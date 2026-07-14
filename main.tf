d5fa1ace3350b386a38ce3336b6b01ed99b5681a
Alok Kumar <alokdnb@users.noreply.github.com>
2026-07-14 15:45:13 -0700

    Add random_pet resource and output for pet_name2

--- main.tf
+++ main.tf
@@ -16,6 +16,11 @@ resource "random_integer" "example" {
   max = 100
 }
 
+resource "random_pet" "example2" {
+  length = 3
+}
+
+
 output "pet_name" {
   value = random_pet.example.id
 }
@@ -23,3 +28,7 @@ output "pet_name" {
 output "lucky_number" {
   value = random_integer.example.result
 }
+
+output "pet_name2" {
+  value = random_pet.example2.id
+} 
